0d3ea0c2724021cc4507cf01b712a4c7d6aa4e94
Şule Nehir ARAÇ <147002677+nehirsule@users.noreply.github.com>
2024-03-07 11:28:24 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //eleman sayısını belirle ve kaydet
+        System.out.print("Fibonacci serisi eleman sayısını girin: ");
+        int n = scanner.nextInt();
+
+        //i sayısından n sayısına kadar belirle ve sisteme dahil et
+        System.out.println("Fibonacci Serisi:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    //döngüyü ve fibonacci formülünü uyugla
+    public static int fibonacci(int n) {
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+        else
+            return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
